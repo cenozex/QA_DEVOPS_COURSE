e8baad8c9601b37c7070ad820e3753ca66aea7ba
cenozex <saugatsiwakoti0925@gmail.com>
2025-12-31 16:07:10 +0545

    started a new backup project with folder structure and finished the config part in script

---
+++ DAY_03/backup_project/scripts/backup.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+echo "=========SCRIPT BY========="
+echo "------------CENOZEX--------"
+
+# ===== CONFIGURATION =====
+BACKUP_DIR="/home/cenozex67/QA-DEVOPS_COURSE/DAY_03/backup_project/backups"
+LOG_FILE="/home/cenozex67/QA-DEVOPS_COURSE/DAY_03/backup_project/logs/backup.log"
+RETENTION_COUNT=5
+DB_NAME="practiceDB"
+
+
